97cc2e9cd755b3a090b1aefcf71ea52c560c36d2
RumyanaDimitrieva <rumyanamm@gmail.com>
2018-05-14 02:22:15 -0700

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.sql.SQLOutput;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root=new Group();
+
+        Text txt=new Text("Sup?");
+        txt.setY(50);
+
+        TextField nameFld=new TextField();
+
+
+
+
+        Button btn=new Button();
+        btn.setText("Say Sup!");
+        btn.setOnAction(evt-> System.out.printf("Sup %s! %n",nameFld.getText()));
+
+        VBox box=new VBox();
+        box.getChildren().addAll(txt,nameFld,btn);
+
+        root.getChildren().add(box);
+        primaryStage.setTitle("Sup");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
